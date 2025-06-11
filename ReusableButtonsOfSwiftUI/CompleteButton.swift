50778618eb65ed20ff5bbd37a636018bbec4fb3c
仲優樹 <yu-ki216m@outlook.jp>
2025-06-11 10:51:30 +0900

    Created CompleteButton.

---
+++ ReusableButtonsOfSwiftUI/CompleteButton.swift
@@ -0,0 +1,18 @@
+//
+//  CompleteButton.swift
+//  ReusableButtonsOfSwiftUI
+//
+//  Created by 仲優樹 on 2025/06/11.
+//
+
+import SwiftUI
+
+struct CompleteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompleteButton()
+}
