8ca45a32c6f57069f8e404d9c1de00734b88462a
仲優樹 <yu-ki216m@outlook.jp>
2025-06-11 10:53:07 +0900

    Created CancelButton.

---
+++ ReusableButtonsOfSwiftUI/CancelButton.swift
@@ -0,0 +1,18 @@
+//
+//  CancelButton.swift
+//  ReusableButtonsOfSwiftUI
+//
+//  Created by 仲優樹 on 2025/06/11.
+//
+
+import SwiftUI
+
+struct CancelButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CancelButton()
+}
