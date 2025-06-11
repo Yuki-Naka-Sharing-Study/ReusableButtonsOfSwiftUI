52235a5de4eb3520e6a66165345a1467f3049bd5
仲優樹 <yu-ki216m@outlook.jp>
2025-06-11 10:40:53 +0900

    Initial Commit

---
+++ ReusableButtonsOfSwiftUI/ReusableButtonsOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReusableButtonsOfSwiftUIApp.swift
+//  ReusableButtonsOfSwiftUI
+//
+//  Created by 仲優樹 on 2025/06/11.
+//
+
+import SwiftUI
+
+@main
+struct ReusableButtonsOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
