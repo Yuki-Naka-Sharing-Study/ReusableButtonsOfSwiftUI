8b4d2120848fc4747ceeb3b514805e851cca460c
仲優樹 <yu-ki216m@outlook.jp>
2025-06-11 10:58:27 +0900

    Created CloseButton.

---
+++ ReusableButtonsOfSwiftUI/CloseButton.swift
@@ -0,0 +1,18 @@
+//
+//  CloseButton.swift
+//  ReusableButtonsOfSwiftUI
+//
+//  Created by 仲優樹 on 2025/06/11.
+//
+
+import SwiftUI
+
+struct CloseButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CloseButton()
+}
