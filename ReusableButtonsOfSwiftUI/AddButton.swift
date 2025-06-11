c2d9527294d5233f8de0b2fc739d454b7f284812
仲優樹 <yu-ki216m@outlook.jp>
2025-06-11 10:49:50 +0900

    Created AddButton.

---
+++ ReusableButtonsOfSwiftUI/AddButton.swift
@@ -0,0 +1,18 @@
+//
+//  AddButton.swift
+//  ReusableButtonsOfSwiftUI
+//
+//  Created by 仲優樹 on 2025/06/11.
+//
+
+import SwiftUI
+
+struct AddButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddButton()
+}
